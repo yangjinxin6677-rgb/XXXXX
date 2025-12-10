cab6adceace1a1871d538501db99f0181b36bf04
yangjinxin6677-rgb <yangjinxin6677@gmail.com>
2025-12-11 01:46:23 +0800

    Create RootLayout component for application layout

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
